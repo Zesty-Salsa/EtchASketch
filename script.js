7e5e6d77baeceeb76f5923a6a43e404e2793aad5
Zeki Salah <salahzekie@gmail.com>
2021-07-31 17:37:10 -0500

    updated to latest version

--- script.js
+++ script.js
@@ -1,60 +1,136 @@
+let button = document.getElementById("changeColors");
+let background = document.getElementById("backgroundContainer");
+let initialColumns = 16;
+let initialRows = 16;
 let container = document.querySelector('.container');
 container.innerHTML="";
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-container.style.gridTemplateRows = 'repeat(16, 1fr)';
-let numberOfColumns = 16;
-let numberOfRows = 16;
 
+//reset slider value on reload
+let slider = document.getElementById("sizeRange");
+slider.value = 16;
+let output = document.getElementById("resolutionValue");
 
-for (i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.style.backgroundColor = 'blue';
-  div.setAttribute('id', ('div' + i));
-  div.setAttribute('class', 'block');
-  container.appendChild(div);
-}
+//display slider value
+output.innerHTML = slider.value + " x " + slider.value; 
+slider.oninput = function() {
+  output.innerHTML = slider.value + " x " + slider.value;  
+} 
 
-function makeSketchable(){
-  let sketchBlocks = document.querySelectorAll(".block");
-  for (let i = 0; i < sketchBlocks.length; i++) {
-    sketchBlocks[i].addEventListener("mouseover", function() {
-      sketchBlocks[i].style.backgroundColor = "purple";
-    });
-  }
-
-  for (let i = 0; i < sketchBlocks.length; i++) {
-    sketchBlocks[i].addEventListener("mouseenter", function() {
-      sketchBlocks[i].style.backgroundColor = "purple";
-      setTimeout(function() {
-        sketchBlocks[i].style.backgroundColor = "blue";
-      }, 50000)
-   });
-  }
-}
-
+//returns new number of columns and rows, deletes old divs, creates new divs and makes them sketchable. 
 function makeGrid(x, y) { 
   let columns = 'repeat(' + y + ', 1fr)';
   let rows = 'repeat(' + x + ', 1fr)';
   container.style.gridTemplateColumns = columns;
-  container.style.gridTemplateRows = rows;
-  
+  container.style.gridTemplateRows = rows;  
   let oldBlocks = document.querySelectorAll(".block");
   for (i = 0; i < oldBlocks.length; i++) {
     oldBlocks[i].remove();
   }
-
   for (i = 0; i < (x * y); i++) {
     const div = document.createElement('div');
-    div.style.backgroundColor = 'blue';
+    div.style.backgroundColor = 'white';
     div.setAttribute('id', ('div' + i));
     div.setAttribute('class', 'block');
     container.appendChild(div);
   }
-  
-  makeSketchable();
-  let numberOfColumns = y;
-  let numberOfRows = x;
-  return columns, rows, numberOfColumns, numberOfRows;
+  if (button.value=="Klick Me 4 Krazy Kolorz"){
+    makeSketchable();
+}
+  else  if (button.value=="Let\'s Calm Down With The Krazy Kolorz"){
+    makeRainbowSketchable();
+  } 
+  else if (button.value=="Back to Black"){
+    makeGreySketchable();
+  }
 }
 
+//add event listener to grid elements
+function makeSketchable(){
+  let clearBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < clearBlocks.length; i++) {
+      clearBlocks[i].replaceWith(clearBlocks[i].cloneNode(true));
+    }
+  let sketchBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < sketchBlocks.length; i++) {
+    sketchBlocks[i].style.backgroundColor = "white";
+    sketchBlocks[i].style.opacity = "0";
+    sketchBlocks[i].addEventListener("mouseover", function makeBlack() {
+      sketchBlocks[i].style.opacity = 1;
+      sketchBlocks[i].style.backgroundColor = "rgba(0, 0, 0, 1)";
+    });
+  }
+}
+
+function makeRainbowSketchable(){
+  let clearBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < clearBlocks.length; i++) {
+      clearBlocks[i].replaceWith(clearBlocks[i].cloneNode(true));
+    }
+  let sketchBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < sketchBlocks.length; i++) {
+    sketchBlocks[i].style.backgroundColor = "white";
+    sketchBlocks[i].style.opacity = "0";
+    sketchBlocks[i].addEventListener("mouseover", function makeRainbow() {
+      sketchBlocks[i].style.opacity = 1;
+      sketchBlocks[i].style.backgroundColor = `hsl(${Math.random() * 360}, 95%, 69%)`;
+    });
+  }
+}
+
+function makeGreySketchable(){
+  let clearBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < clearBlocks.length; i++) {
+      clearBlocks[i].replaceWith(clearBlocks[i].cloneNode(true));
+    }
+  let sketchBlocks = document.querySelectorAll(".block");
+  for (let i = 0; i < sketchBlocks.length; i++) {
+    sketchBlocks[i].style.backgroundColor = "white";
+    sketchBlocks[i].style.opacity = "0";
+    sketchBlocks[i].addEventListener("mouseover", function makeGrey() {
+      var currentOpacity = sketchBlocks[i].style.opacity;
+      if (currentOpacity == "") {
+        let newOpacity = 0.1;
+      }
+      else { 
+        var newOpacity = Number(currentOpacity) + 0.1;
+      }
+     sketchBlocks[i].style.opacity = newOpacity;
+     sketchBlocks[i].style.backgroundColor = "black";
+   });
+  }
+}
+
+slider.addEventListener("change", function() {
+  var value = document.getElementById("sizeRange").value;
+  console.log(value);
+  makeGrid(value, value);
+})
+
+
+function toggle(button)
+{
+  if (button.value=="Klick Me 4 Krazy Kolorz"){
+   button.value="Let\'s Calm Down With The Krazy Kolorz";
+    button.style.background="#808080";
+    backgroundContainer.classList.add("rainbow");
+    backgroundContainer.classList.remove("default");
+    makeRainbowSketchable();
+  } 
+  else  if (button.value=="Let\'s Calm Down With The Krazy Kolorz"){
+   button.value="Back to Black";
+    button.style.background="#000000";
+    backgroundContainer.classList.add("grey");
+    backgroundContainer.classList.remove("rainbow");
+    makeGreySketchable();
+  }  
+  else if (button.value=="Back to Black"){
+   button.value="Klick Me 4 Krazy Kolorz";
+    button.style.background="linear-gradient(to bottom right, red,orange,yellow,green,blue,indigo,violet)";
+    backgroundContainer.classList.add("default");
+    backgroundContainer.classList.remove("grey");
+    makeSketchable();
+  }
+}
+
+makeGrid(initialColumns, initialRows);
 makeSketchable();
